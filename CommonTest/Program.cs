4d95677eb1c5f79f7055718c4ab75f9d18562a17
chris ren <rj532029887@live.com>
2017-04-05 18:36:12 +0800

    增加vuejs过滤器

--- CommonTest/Program.cs
+++ CommonTest/Program.cs
@@ -10,11 +10,29 @@ namespace CommonTest
 {
     class Program
     {
-        static int[] arr = new int[] { 1, 2, 3, 4, 5 };
-        static void Main(string[] args)
+        class SwappingStrings
         {
-            
-            Console.ReadLine();
+            static void SwapStrings(int[] arr1, int[] arr2)
+            // The string parameter is passed by reference.
+            // Any changes on parameters will affect the original variables.
+            {
+                int[] temp = { 1 };
+                arr1 = arr2;
+                arr2 = temp;
+                System.Console.WriteLine("Inside the method: {0} {1}", arr1[0], arr2[0]);
+            }
+
+            static void Main()
+            {
+                int[] arr1 = { 1 };
+                int[] arr2 = { 2};
+                System.Console.WriteLine("Inside Main, before swapping: {0} {1}", arr1[0], arr2[0]);
+
+                SwapStrings(arr1, arr2);   // Passing strings by reference
+                System.Console.WriteLine("Inside Main, after swapping: {0} {1}", arr1[0], arr2[0]);
+
+                Console.Read();
+            }
         }
     }
 }
